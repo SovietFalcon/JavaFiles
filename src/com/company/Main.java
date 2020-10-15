e1a6555ffd01f140e53c7e3577b0484369a05ae3
SovietFalcon <ivancp3000@hotmail.es>
2020-10-15 16:26:21 +0200

    -

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        MostraFitxers test = new MostraFitxers();
+        System.out.println("Aquí no hay nada");
+        // . . . .
 
 
     }
